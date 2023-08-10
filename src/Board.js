7db7177e8ad90d8063fe8a6f9d3a7f28394ac940
Dennis Cahillane <dennis@denniscahillane.info>
2023-08-10 10:49:57 -0400

    wip

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 function Board() {
+  const [board, setBoard] = useState(
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  );
+  const [isXNext, setIsXNext] = useState(true);
+  const handleCellClick = (row, col) => {
+    if (board[row][col] !== null) return; // cell already filled
+    
+    const newBoard = board.map((r, rowIndex) => 
+      r.map((c, colIndex) => 
+        rowIndex === row && colIndex === col ? (isXNext ? 'X' : 'O') : c
+      )
+    );
+
+    setBoard(newBoard);
+    setIsXNext(!isXNext); // Toggle the turn
+  }
+
   return (
     <div>
-      insert board
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          -----------------
+          {row.map((cell, colIndex) => (
+            <button 
+              key={colIndex}
+              onClick={() => handleCellClick(rowIndex, colIndex)}
+            >
+              |{cell || 'null'}|
+            </button>
+          ))}
+        </div>
+      ))}
     </div>
   )
 }

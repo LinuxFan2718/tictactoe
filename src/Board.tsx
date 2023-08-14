8eb96e0451b402e0e9f76604d7b484b4159b5bfd
Dennis Cahillane <dennis@denniscahillane.info>
2023-08-13 23:33:46 -0400

    check winner immediately after player move

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import random_bot from './bots/random_bot';
 import { GameState } from './types/GameState';
+import { BoardType } from "./BoardType";
 
 interface BoardProps {
   gameState: GameState;
@@ -11,26 +12,7 @@ interface BoardProps {
 function Board({gameState, setGameState}: BoardProps) {
 
   const handleCellClick = (row: number, col: number) => {
-    if(gameState.kind === 'InProgress' && gameState.playertype[gameState.turn] === 'human') {
-      if (gameState.board[row][col] === null) {
-        let newBoard = structuredClone(gameState.board);
-        newBoard[row][col] = gameState.turn;
-
-        setGameState(
-          {
-            kind: "InProgress",
-            turn: gameState.turn === 'X' ? 'O' : 'X',
-            playertype: gameState.playertype,
-            board: newBoard
-          })
-      }
-    }
-  }
-
-  useEffect(() => {
-    function checkWinner(): 'X' | 'O' | null {
-      if (gameState.kind !== 'InProgress') return null;
-      let board = gameState.board;
+    function checkWinner(board: BoardType): 'X' | 'O' | null {
       for (let i = 0; i < 3; i++) {
         if (board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) return board[i][0];
         if (board[0][i] && board[0][i] === board[1][i] && board[0][i] === board[2][i]) return board[0][i];
@@ -41,21 +23,58 @@ function Board({gameState, setGameState}: BoardProps) {
       
       return null;
     }
-    function checkDraw() {
-      if (gameState.kind !== 'InProgress') return false;
+    function checkDraw(board: BoardType) {
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j< 3; j++) {
-          if (gameState.board[i][j] === null) return false;
+          if (board[i][j] === null) return false;
         }
       }
       return true;
     };
 
-    let winner = checkWinner();
-    if (winner) {
-      setGameState({ kind: 'Finished', winner: winner, board: gameState.board });
-    } else if (checkDraw()) {
-      setGameState({ kind: 'Finished', winner: null, board: gameState.board });
+    if(gameState.kind === 'InProgress' && gameState.playertype[gameState.turn] === 'human') {
+      if (gameState.board[row][col] === null) {
+        let newBoard = structuredClone(gameState.board);
+        newBoard[row][col] = gameState.turn;
+
+        let winner = checkWinner(newBoard);
+        if (winner) {
+          setGameState({ kind: 'Finished', winner: winner, board: newBoard });
+        } else if (checkDraw(newBoard)) {
+          setGameState({ kind: 'Finished', winner: null, board: newBoard });
+        } else {
+          setGameState(
+            {
+              kind: "InProgress",
+              turn: gameState.turn === 'X' ? 'O' : 'X',
+              playertype: gameState.playertype,
+              board: newBoard
+            }
+          )
+        };
+      }
+    }
+  }
+
+  useEffect(() => {
+    function checkWinner(board: BoardType): 'X' | 'O' | null {
+      for (let i = 0; i < 3; i++) {
+        if (board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) return board[i][0];
+        if (board[0][i] && board[0][i] === board[1][i] && board[0][i] === board[2][i]) return board[0][i];
+      }
+      
+      if (board[0][0] && board[0][0] === board[1][1] && board[0][0] === board[2][2]) return board[0][0];
+      if (board[0][2] && board[0][2] === board[1][1] && board[0][2] === board[2][0]) return board[0][2];
+      
+      return null;
+    }
+    function checkDraw(board: BoardType) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j< 3; j++) {
+          if (board[i][j] === null) return false;
+        }
+      }
+      return true;
     };
 
     if (gameState.kind === 'InProgress' && gameState.playertype[gameState.turn] === 'bot') {
@@ -64,14 +83,21 @@ function Board({gameState, setGameState}: BoardProps) {
       let newBoard = structuredClone(gameState.board);
       newBoard[row][col] = gameState.turn;
 
-      setGameState(
-        {
-          kind: "InProgress",
-          turn: gameState.turn === 'X' ? 'O' : 'X',
-          playertype: gameState.playertype,
-          board: newBoard
-        }
-      )
+      let winner = checkWinner(newBoard);
+      if (winner) {
+        setGameState({ kind: 'Finished', winner: winner, board: newBoard });
+      } else if (checkDraw(newBoard)) {
+        setGameState({ kind: 'Finished', winner: null, board: newBoard });
+      } else {
+        setGameState(
+          {
+            kind: "InProgress",
+            turn: gameState.turn === 'X' ? 'O' : 'X',
+            playertype: gameState.playertype,
+            board: newBoard
+          }
+        )
+      };
     }
   }, [gameState, setGameState]);
 
